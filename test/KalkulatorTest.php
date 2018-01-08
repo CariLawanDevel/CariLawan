d93454b7b2529011aeb32ca3cc5f5638351184e2
NikkoES <nikkoeka04@gmail.com>
2018-01-08 20:13:30 +0700

    percobaan CI #5

--- test/KalkulatorTest.php
+++ test/KalkulatorTest.php
@@ -4,7 +4,7 @@ class KalkulatorTest extends PHPUnit_Framework_TestCase {
  
     public function testKalkulator()
     {
-    	include '../Kalkulator.php';
+    	include '/../Kalkulator.php';
         $this->assertEquals(444, tambah(123,321));
         $this->assertEquals(80, kurang(100,20));
         $this->assertEquals(49, kali(7,7));
